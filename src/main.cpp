8b9ab4af0470f64c36d877f18fc2be0e2def34ab
Andrei Grosu <andrei_grosu@fastmail.net>
2016-09-15 12:37:21 +0300

    Refrigerator accepts boiling temperatures !

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,6 @@
 
 #include "tools.h"
 
-/*
- *  Here we declare the INTERFACE of our Refrigerator type.
- *  There are only member function declarations, no implementation code.
- *
- *  Other programmers that want to use Refrigerators in their code only
- *  need to look at this class declaration and understand HOW a Refrigerator
- *  can be used. They don't need to see the details of how start() or stop()
- *  or any other methods work.
- *
- *  So far we gave only the 'outline' of how Refrigerators are used.
- *  Notice there are no member variables in the refrigerator class so far.
- */
-
 /** \brief A class that models a Refrigerator.
  *
  */
@@ -93,28 +80,15 @@ int main(int argc, char * argv[]) {
 		goto done;
 	}
 
-	// There is a software development paradigm called TEST-DRIVEN DEVELOPMENT (TDD) which
-	// we are employing here.
-	// In this paradigm, all types (classes) we create must be covered by tests such that
-	// if we change the implementation of a class, we can run these tests and see if
-	// we broke something.
-	//
-	// One school of thought in the TDD community sais that it is better to write the tests
-	// first for a given class and then implement the functionality in that class.
-	// That is, we imagine the possible uses of our new class and write a test program
-	// that should cover all these usages and then we procede to implement the class such
-	// that our test program completes correctly.
-	//
-	// We've done the first two tests along with their implementation so now let's try
-	// writing in advance all unit tests for our refrigerator and then implement the
-	// functionality.
-
 	// Test that we can change the temperature of a refrigerator:
 	passed = passed or test_change_refrigerator_temperature(test_fridge);
 	if(not passed) {
 		std::cout << "TEST FAILED : can set refrigerator temperature !" << std::endl;
 		goto done;
 	}
+
+	// however this is not quite ok ... let's look at the temperature test
+	// function to see why:
 
 	// Test that a working refrigerator can be stopped:
 	passed = passed or test_stop_refrigerator(test_fridge);
@@ -128,9 +102,6 @@ int main(int argc, char * argv[]) {
 done:
 	return (passed)?(EXIT_SUCCESS):(EXIT_FAILURE);
 }
-
-// Implementation of test functions goes here so there are not in our way when we look at what
-// main() does:
 
 /**
  * Tests if the given refrigerator is not working.
@@ -151,9 +122,21 @@ bool test_start_refrigerator(refrigerator & r) {
  * Test that a change in temperature is actually performed.
  */
 bool test_change_refrigerator_temperature(refrigerator & r) {
+	bool passed = false;
+
+	// here I am setting a 'valid' temperature, that is something in line
+	// with what a real refrigerator would accept in order to function properly:
 	const int freezing = -10;
 	r.set_temperature(freezing);
-	return (freezing == r.get_temperature());
+	passed = (freezing == r.get_temperature());
+
+	// however a real user of our refrigerator class could as well do this
+	// and, as you can see, the tests still succeed !
+  const int boiling = 100;
+	r.set_temperature(boiling);
+	passed = (boiling == r.get_temperature());
+
+	return passed;
 }
 
 /**
